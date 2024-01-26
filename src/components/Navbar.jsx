fad08403c42014eb720b04caf3e163ebba4179b8
ngaeL <kimleangn@gmail.com>
2024-01-26 08:46:40 -0800

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
 
 
 
 const Navbar = () => {
     return(
         <div className="navbar">
-            <div className="navbar_box">
+            <div className="navbar_menu">
+                <Link to="/" className="navbar_menu_button">
+                    HOME
+                </Link>
+                <Link to="/tasks" className="navbar_menu_button">
+                    T/E/A
+                </Link>
+                <Link to="/assigntasks" className="navbar_menu_button">
+                    ASSIGN
+                </Link>
+            </div>
+
+            {/*
+             <div className="navbar_box">
                 <div className="navbar_box_line">
 
                 </div>
@@ -15,6 +29,9 @@ const Navbar = () => {
 
                 </div>
             </div>
+            */}
+           
+          
         </div>
     )
 }
